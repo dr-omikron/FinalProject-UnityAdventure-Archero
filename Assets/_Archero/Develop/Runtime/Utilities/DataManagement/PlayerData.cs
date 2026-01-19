518e95d6ee71e822475ecc78195fab507c950cfd
dr-omikron <76596650+dr-omikron@users.noreply.github.com>
2026-01-19 17:50:03 +0400

    created MapDataKeyStorage

--- Assets/_Archero/Develop/Runtime/Utilities/DataManagement/PlayerData.cs
+++ Assets/_Archero/Develop/Runtime/Utilities/DataManagement/PlayerData.cs
@@ -4,7 +4,7 @@ using _Archero.Develop.Runtime.Meta.Features.Wallet;
 
 namespace _Archero.Develop.Runtime.Utilities.DataManagement
 {
-    public class PlayerData
+    public class PlayerData : ISaveData
     {
         public Dictionary<CurrencyType, int> WalletData;
     }
