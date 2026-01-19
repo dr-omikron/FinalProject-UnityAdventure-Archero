dc33404ab50de3485895dc15a7339ab700f37eea
dr-omikron <76596650+dr-omikron@users.noreply.github.com>
2026-01-19 17:08:03 +0400

    created PlayerData

---
+++ Assets/_Archero/Develop/Runtime/Utilities/DataManagement/PlayerData.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Collections.Generic;
+using _Archero.Develop.Runtime.Meta.Features.Wallet;
+
+namespace _Archero.Develop.Runtime.Utilities.DataManagement
+{
+    public class PlayerData
+    {
+        public Dictionary<CurrencyType, int> WalletData;
+    }
+}
