72d6337a6db33225edc83f7b4ed331fc2d4040f6
dr-omikron <76596650+dr-omikron@users.noreply.github.com>
2026-02-03 22:22:18 +0400

    Created Entity.cs and Components

---
+++ Assets/_Archero/Develop/Runtime/Gameplay/EntitiesCore/Entity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Archero.Develop.Runtime.Gameplay.EntitiesCore
+{
+    public class Entity
+    {
+        private readonly Dictionary<Type, IEntityComponent> _components = new Dictionary<Type, IEntityComponent>();
+
+        public void AddComponent<TComponent>(TComponent component) where TComponent : class, IEntityComponent
+        {
+            _components.Add(typeof(TComponent), component);
+        }
+
+        public bool HasComponent<TComponent>() where TComponent : class, IEntityComponent
+            => _components.ContainsKey(typeof(TComponent));
+
+        public bool TryGetComponent<TComponent>(out TComponent component) where TComponent : class, IEntityComponent
+        {
+            if (_components.TryGetValue(typeof(TComponent), out IEntityComponent foundObject))
+            {
+                component = (TComponent)foundObject;
+                return true;
+            }
+
+            component = null;
+            return false;
+        }
+
+        public TComponent GetComponent<TComponent>() where TComponent : class, IEntityComponent
+        {
+            if (TryGetComponent(out TComponent component) == false)
+                throw new ArgumentException($"Entity not exist {typeof(TComponent)}");
+
+            return component;
+        }
+    }
+}
---
+++ Assets/_Archero/Develop/Runtime/Gameplay/Features/MovementFeature/MovementComponents.cs
@@ -0,0 +1,21 @@
+﻿using _Archero.Develop.Runtime.Gameplay.EntitiesCore;
+using _Archero.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace _Archero.Develop.Runtime.Gameplay.Features.MovementFeature
+{
+    public class MoveDirection : IEntityComponent
+    {
+        public ReactiveVariable<Vector3> Value;
+    }
+
+    public class MoveSpeed : IEntityComponent
+    {
+        public ReactiveVariable<float> Value;
+    }
+
+    public class RigidbodyComponent : IEntityComponent
+    {
+        public Rigidbody Value;
+    }
+}
